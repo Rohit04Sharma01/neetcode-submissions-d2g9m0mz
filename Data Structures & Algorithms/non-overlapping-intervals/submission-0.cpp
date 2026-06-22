3eb4a10007d5998dbe0ee9e022e56e639ee3209b
Rohit Sharma <116329870+Rohit04Sharma01@users.noreply.github.com>
2026-06-22 12:54:02 +0530

    Add: non-overlapping-intervals - submission-0

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end(), [] (auto& a, auto& b) {
+            return a[1] < b[1];
+        });
+        int count = 0;
+        int prevEnd = intervals[0][1];
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] < prevEnd) {
+                count += 1;
+            } else {
+                prevEnd = intervals[i][1];
+            }
+        }
+        return count;
+    }
+};
